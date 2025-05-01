10002830c52ed58712258dcfcc9954ea5bdc4446
ArtemShkuratov-qa <artem.shkuratov001@gmail.com>
2025-05-01 17:47:49 +0300

    add project

--- test_demoqa.py
+++ test_demoqa.py
@@ -1,12 +1,13 @@
+import allure
+
 from qa_guru_hw_11.data import users
-from qa_guru_hw_11.data.users import User, student
 from qa_guru_hw_11.pages.registration_page import RegistrationPage
-from datetime import date
 
 def test_demo_qa():
     registration_page = RegistrationPage()
     test_profile = users.student
-    registration_page.open()
+    with allure.step("Открываем главую страницу GitHub"):
+        registration_page.open()
     registration_page.register(test_profile)
     registration_page.should_have_data(test_profile)
 
