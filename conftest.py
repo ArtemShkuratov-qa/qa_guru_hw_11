3cf86487b916c202c498a5d7640ff7658c3c2698
ArtemShkuratov-qa <artem.shkuratov001@gmail.com>
2025-05-07 16:46:10 +0300

    add parser and default settings

--- conftest.py
+++ conftest.py
@@ -8,12 +8,24 @@ from utils import attach
 from dotenv import load_dotenv
 import os
 
+DEFAULT_BROWSER_VERSION = "127.0"
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='127.0'
+    )
+
+
 @pytest.fixture(scope="session", autouse=True)
 def load_env():
     load_dotenv()
 
-@pytest.fixture(scope='function', autouse=True)
+
+@pytest.fixture(scope='function')
 def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -24,9 +36,11 @@ def setup_browser(request):
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
     selenoid_login = os.getenv('SELENOID_LOGIN')
     selenoid_pass = os.getenv('SELENOID_PASS')
     selenoid_url = os.getenv('SELENOID_URL')
+
     driver = webdriver.Remote(
         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
