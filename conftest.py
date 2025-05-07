70e0d7ef764ff4666291cc0d19d735ae9edbacbb
ArtemShkuratov-qa <artem.shkuratov001@gmail.com>
2025-05-07 22:22:09 +0300

    fix conftest

--- conftest.py
+++ conftest.py
@@ -8,18 +8,12 @@ from utils import attach
 from dotenv import load_dotenv
 import os
 
-DEFAULT_BROWSER_VERSION = "127.0"
-DEFAULT_BROWSER_NAME = 'chrome'
+DEFAULT_BROWSER_VERSION = "128.0"
 
 def pytest_addoption(parser):
     parser.addoption(
         '--browser_version',
-        default='127.0'
-    )
-
-    parser.addoption(
-        '--browser_name',
-        default='chrome'
+        default='128.0'
     )
 
 
@@ -32,16 +26,14 @@ def load_env():
 def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    browser_name = request.config.getoption('--browser_name')
-    browser_name = browser_name if browser_name != "" else DEFAULT_BROWSER_NAME
     options = Options()
     selenoid_capabilities = {
-        "browserName": browser_name,
+        "browserName": 'chrome',
         "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True,
-            "screenResolution": "1920x1080x24"
+            "screenResolution": '1920x1080x24'
         }
     }
     options.capabilities.update(selenoid_capabilities)
